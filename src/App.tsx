7d04f65180bbf1fe573926d00405877d41bf4dc8
SaddamHilles <sdm.hilles@gmail.com>
2024-05-21 14:25:22 +0300

    auth

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import CityList from './components/CityList/CityList';
 import CountryList from './components/CountryList/CountryList';
 import City from './components/City/City';
 import { CitiesProvider } from './contexts/CitiesProvider';
+import { AuthProvider } from './contexts/AuthProvider';
 // import AppLayout from './pages/AppLayout';
 const Homepage = lazy(() => import('./pages/Home/Homepage'));
 const Product = lazy(() => import('./pages/Product/Product'));
@@ -21,31 +22,33 @@ const PageNotFound = lazy(() => import('./pages/PageNotFound/PageNotFound'));
 
 const App = () => {
   return (
-    <CitiesProvider>
-      <BrowserRouter>
-        <Suspense fallback={<SpinnerFullPage />}>
-          <Routes>
-            <Route index element={<Homepage />} />
-            <Route path='product' element={<Product />} />
-            <Route path='pricing' element={<Pricing />} />
-            <Route path='login' element={<Login />} />
-            <Route path='app' element={<AppLayout />}>
-              <Route index element={<Navigate to={'cities'} replace />} />
-              <Route path='cities' element={<CityList />} />
-              <Route path='cities/:id' element={<City />} />
-              <Route path='countries' element={<CountryList />} />
-              {/* <Route path="*" element={<p>City Not found!</p>} /> */}
-              <Route path='form' element={<Form />} />
-            </Route>
-            {/* <Route index element={<Navigate replace to="cities" />} /> */}
-            {/* <Route path="cities" element={<CityList />} /> */}
-            {/* <Route path="cities/:id" element={<City />} /> */}
-            {/* <Route path="countries" element={<CountryList />} /> */}
-            <Route path='*' element={<PageNotFound />} />
-          </Routes>
-        </Suspense>
-      </BrowserRouter>
-    </CitiesProvider>
+    <AuthProvider>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Suspense fallback={<SpinnerFullPage />}>
+            <Routes>
+              <Route index element={<Homepage />} />
+              <Route path='product' element={<Product />} />
+              <Route path='pricing' element={<Pricing />} />
+              <Route path='login' element={<Login />} />
+              <Route path='app' element={<AppLayout />}>
+                <Route index element={<Navigate to={'cities'} replace />} />
+                <Route path='cities' element={<CityList />} />
+                <Route path='cities/:id' element={<City />} />
+                <Route path='countries' element={<CountryList />} />
+                {/* <Route path="*" element={<p>City Not found!</p>} /> */}
+                <Route path='form' element={<Form />} />
+              </Route>
+              {/* <Route index element={<Navigate replace to="cities" />} /> */}
+              {/* <Route path="cities" element={<CityList />} /> */}
+              {/* <Route path="cities/:id" element={<City />} /> */}
+              {/* <Route path="countries" element={<CountryList />} /> */}
+              <Route path='*' element={<PageNotFound />} />
+            </Routes>
+          </Suspense>
+        </BrowserRouter>
+      </CitiesProvider>
+    </AuthProvider>
   );
 };
 
--- src/contexts/AuthProvider.tsx
+++ src/contexts/AuthProvider.tsx
@@ -1,6 +1,60 @@
+import { createContext, useContext, useState } from 'react';
 
-function AuthProvider() {
-  return <div>AuthProvider</div>;
+export interface User {
+  email: string;
+  password: string;
+}
+interface Auth {
+  user: User;
+  isAuth: boolean;
+  login: (userInfo: User) => void;
+  logout: () => void;
+}
+const AuthContext = createContext({} as Auth);
+
+interface Props {
+  children: React.ReactNode;
 }
 
-export default AuthProvider;
+const FAKE_USER = {
+  name: 'Jack',
+  email: 'jack@example.com',
+  password: 'qwerty',
+  avatar: 'https://i.pravatar.cc/100?u=zz',
+};
+
+function AuthProvider({ children }: Props) {
+  const [user, setUser] = useState<User>({} as User);
+  const [isAuth, setIsAuth] = useState(false);
+
+  function login(userInfo: User) {
+    console.log('userInfo: ', userInfo);
+    if (
+      userInfo.email === FAKE_USER.email &&
+      userInfo.password === FAKE_USER.password
+    ) {
+      setUser(userInfo);
+      setIsAuth(true);
+    }
+  }
+  function logout() {
+    setUser({ email: '', password: '' });
+    setIsAuth(false);
+  }
+  const auth: Auth = {
+    user,
+    login,
+    logout,
+    isAuth,
+  };
+  return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
+}
+
+function useAuth() {
+  const ctxAuth = useContext(AuthContext);
+  if (!ctxAuth) {
+    throw new Error("You can't use AuthContext outside the AuthProvider");
+  }
+  return ctxAuth;
+}
+export { AuthProvider, useAuth };
